32adecc25f2703f0831739b075af731142317345
Tirokaitube <tirokaitube@gmail.com>
2025-04-22 14:00:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "5953621837"))
 
 
 # Premium 4GB Renaming Client Config
