e1e28540d11598cba236020eb84af562feb56a3c
Tirokaitube <tirokaitube@gmail.com>
2025-04-22 13:57:24 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,10 +20,10 @@ class script(object):
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
 <b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
+<b>📢 Channel :</b> <a href='https://t.me/Anime_Lumino'>Anime_Lumino Botz</a>
 <b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
 
-<b>♻️ Bot Made By :</b> @Madflix_Bots"""
+<b>♻️ Bot Made By :</b> @Anime_Lumino"""
 
     DONATE_TXT = """
 <b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
@@ -50,9 +50,9 @@ If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upt
 
 For Example :-
 
-<code>By @Madflix_Bots</code>
+<code>By @Anime_Lumino</code>
 
-💬 For Any Help Contact @MadflixOfficials
+💬 For Any Help Contact @Anime_Lumino
 """
 
 
